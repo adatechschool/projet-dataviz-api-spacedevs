cbdcf6c5e4f0a7bf23a32f5bc91fdb06e133a06f
dvniel <nexus@andromeda.local>
2024-11-28 00:07:11 +0100

    de légères modifications apportées

--- dom.js
+++ dom.js
@@ -1,29 +1,25 @@
-export const FIRST_USER_CHOICE = ''
-export const SECOND_USER_CHOICE = ''
-
 /** Selection des conteneur pour APOD ( 'APOD' => photo du jour ) */
-
 export const body = document.getElementById('body');
 export const headingApod = document.querySelector('h1')
 export const apodContainer = document.getElementById('apod-container');
-export const apod = document.getElementById('APOD')
-export const titleApod = document.getElementById('title-apod');
-export const explanationApod = document.getElementById('explanation-apod');
-export const dateApod = document.getElementById('date-apod');
-export const imageApod = document.getElementById('img-apod');
-/** toggle button */
+
+/** BOUTONS */
 export const toggleButton = document.getElementById('toggle-button');
-export const optionsContainer = document.getElementById('options-container');
-export const dateSearch = document.getElementById('date-search');
-export const dateRangeSearch = document.getElementById('date-range-search');
-export const eraseButton = document.getElementById('erase-button');
+export const buttonContainer = document.getElementById('buttons-container');
+export const apodButton = document.getElementById('button-apod');
+export const searchDateButton = document.getElementById('button-search-date');
+export const searchRangeButton = document.getElementById('button-search-range');
+export const countButton = document.getElementById('button-count');
+export const thumbsButton = document.getElementById('button-thumbs');
+export const eraseButton = document.getElementById('button-erase');
+export const resultContainer = document.getElementById('results-container');
+resultContainer.innerHTML = '';
+
+/** Message de chargement */
 export const loadingMessage = document.createElement('h2');
-optionsContainer.appendChild(loadingMessage);
+resultContainer.appendChild(loadingMessage);
 loadingMessage.style.display = 'none'
 loadingMessage.innerText = 'Loading ...';
 loadingMessage.style.fontSize = '30px';
 loadingMessage.style.color = 'white';
 
-export const API_KEY = "w1qZiSyElNKvBmMGpk3BYjIsrJSzpqz3caMVp90b";
-export const URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
-export const BASE_URL_API = `https://api.nasa.gov/planetary/apod`
