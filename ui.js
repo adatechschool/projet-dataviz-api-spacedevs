e512d6ae9cec1b51d45aadc9481f2c4eeacfea38
Joe19071991 <lauranove66@gmail.com>
2024-11-29 15:13:32 +0100

    rectification

--- ui.js
+++ ui.js
@@ -1,40 +1,32 @@
 import { fetchData } from "./api.js";
 import { buttonDisplayRange, displayContainer } from "./dom.js";
-const apodContainer = document.getElementById('apod-container');
-
-let userChoiceDate;
-let startDate;
-let endDate;
-
-
+const apodContainer = document.getElementById("apod-container");
 
 const loadApod = async (type) => {
   try {
     const data = await fetchData(type);
-    apodContainer.innerHTML = 
-  `<h1>${data.date}</h1>
+    apodContainer.innerHTML = `<h1>${data.date}</h1>
   <h2>${data.title}</h2>
   <img src=${data.url}>
-  <p>${data.explanation}</p>`
-
+  <p>${data.explanation}</p>`;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
-loadApod('apod');
+loadApod("apod");
 
-buttonDisplayRange.addEventListener('click', async () => {
-  const inputStartDate = document.getElementById('input-start-date').value;
-  const inputEndDate = document.getElementById('input-end-date').value;
+buttonDisplayRange.addEventListener("click", async () => {
+  const inputStartDate = document.getElementById("input-start-date").value;
+  const inputEndDate = document.getElementById("input-end-date").value;
   const startDate = inputStartDate;
   const endDate = inputEndDate;
   console.log(startDate, endDate);
 
   try {
-    const data = await fetchData('range', startDate, endDate);
-      data.forEach(item => {
-        displayContainer.innerHTML += `
+    const data = await fetchData("range", startDate, endDate);
+    data.forEach((item) => {
+      displayContainer.innerHTML += `
           <div class="data-item">
             <h3>${item.title}</h3>
             <p><strong>Date:</strong> ${item.date}</p>
@@ -42,11 +34,10 @@ buttonDisplayRange.addEventListener('click', async () => {
             <p>${item.explanation}</p>
           </div>
         `;
-      });
+    });
   } catch (error) {
-    console.error('Erreur dans fetchData:', error);
-    displayContainer.innerHTML = '<p>Une erreur est survenue lors du chargement des données.</p>';
+    console.error("Erreur dans fetchData:", error);
+    displayContainer.innerHTML =
+      "<p>Une erreur est survenue lors du chargement des données.</p>";
   }
 });
-
-
