6c9ff9e67a45f89e59636f16ddac5081bd3847f4
Joe19071991 <lauranove66@gmail.com>
2024-12-02 10:41:54 +0100

    création du bouton white mode

--- js/dom.js
+++ js/dom.js
@@ -1,10 +1,26 @@
 // INDEX.HTML
-export const homeScreen = document.getElementById('hoem-screen');
+export const homeScreen = document.getElementById("hoem-screen");
 // GALERIE.HTML
 
 // SEARCH-BY-DATE.HTML
-export const buttonDisplayRange = document.getElementById('button-display-range');
-export const  displayContainer = document.getElementById('display-container') 
+export const buttonDisplayRange = document.getElementById(
+  "button-display-range"
+);
+export const displayContainer = document.getElementById("display-container");
 
 // RANDOM
-export const randomButton = document.getElementById('random-button');
+export const randomButton = document.getElementById("random-button");
+
+const button = document.querySelector("#toggleTheme");
+const body = document.querySelector("body");
+let isDarkMode = true;
+
+button.addEventListener("click", () => {
+  isDarkMode = !isDarkMode;
+  isDarkMode ? update("black", "white") : update("white", "black");
+});
+
+function update(bgColor, textColor) {
+  body.style.backgroundColor = bgColor;
+  body.style.color = textColor;
+}
